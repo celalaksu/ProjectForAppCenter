e23fa54c75e239092ebc9023f78e9daa97a50205
celalaksu <32665644+celalaksu@users.noreply.github.com>
2020-06-26 18:39:11 +0300

    New share future

--- Finance/App.xaml.cs
+++ Finance/App.xaml.cs
@@ -2,6 +2,9 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Finance.View;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Analytics;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace Finance
@@ -15,9 +18,18 @@ namespace Finance
             MainPage = new NavigationPage(new MainPage());
         }
 
-        protected override void OnStart()
+        protected override async void OnStart()
         {
             // Handle when your app starts
+            string androidAppSecret = "e92cb974-cb34-4766-b92d-f4c0ad666f50";
+            string iOSAppSecret = "fc1cd97a-fa60-4e29-a745-645fbdba754f";
+            AppCenter.Start($"android={androidAppSecret};ios={iOSAppSecret}", typeof(Crashes), typeof(Analytics));
+
+            bool didAppCrashed = await Crashes.HasCrashedInLastSessionAsync();
+            if (didAppCrashed)
+            {
+                var crashReport = await Crashes.GetLastSessionCrashReportAsync();
+            }
         }
 
         protected override void OnSleep()
